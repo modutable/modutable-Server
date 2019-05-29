0951448f4c08fd03e2945bb20ed18a24d39f631a
Inyong Jung <jiy8319@gmail.com>
2019-05-30 00:23:46 +0900

    get address data by search keyword in mainView

--- src/controller/host.ts
+++ src/controller/host.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
 import "reflect-metadata";
-import { getRepository } from "typeorm";
+import { getRepository, Like } from "typeorm";
 import { hosts } from "../entity/hosts";
 
 export = {
   mainAddress: async (req: Request, res: Response) => {
-    const table = await getRepository(hosts).find();
+    const { searchAddress } = req.body;
+    const table = await getRepository(hosts)
+      .createQueryBuilder("hosts")
+      .select(["hosts.address"])
+      .where("hosts.address like :searchKeyword", {
+        searchKeyword: `%${searchAddress}%`
+      })
+      .getMany();
     res.json(table);
   }
 };
