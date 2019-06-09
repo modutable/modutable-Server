c01db661e13f51c97b8f069352dc1bb8f86d7556
Inyong Jung <jiy8319@gmail.com>
2019-06-09 13:05:35 +0900

    set send mail for change password

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import eventsRouter from "./events";
 import messageRouter from "./message";
+import mailController from "../controller/mail";
+import { checkToken } from "../middleware/tokenparser";
+
+require("dotenv").config();
 
 const router = express.Router();
 
 router.use("/events", eventsRouter);
 router.use("/message", messageRouter);
+
+router.post("/mail", checkToken, mailController.sendMail);
 
 export = router;
