e14c0b2d66a2b166fc0796810d6bf1995e45833a
Johnny Koo <johnnykoo84@gmail.com>
2019-06-12 18:58:41 +0900

    Initial Code Review

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { createConnection, Connection } from "typeorm";
 
 export const ORMConnect = () => {
-  createConnection().then(async connection => {
+  createConnection().then(async connection => { // connection not used?
     try {
       console.log("TypeORM Start");
     } catch (e) {
