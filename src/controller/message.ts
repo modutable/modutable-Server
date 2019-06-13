a1ca7f43cb71c843b2046c7f3f88e2a8908884de
Inyong Jung <jiy8319@gmail.com>
2019-06-13 12:38:07 +0900

    just completed message Socket network, and save database

--- src/controller/message.ts
+++ src/controller/message.ts
@@ -4,19 +4,15 @@ import { getRepository } from "typeorm";
 import { Messages } from "../entity/Messages";
 import { Users } from "../entity/Users";
 import { check } from "../middleware/tokenparser";
-import { MetadataArgsStorage } from "typeorm/metadata-args/MetadataArgsStorage";
 export = {
   save: async (data: any) => {
-    const fromUser = await check(data.from);
-    const toUser = await getUserIdByEmail(data.to);
-
     const newMessage = new Messages();
-    newMessage.sendUserId = JSON.parse(JSON.stringify(fromUser)).id;
-    newMessage.getUserId = JSON.parse(JSON.stringify(toUser)).id;
+    newMessage.sendUserId = data.sendUserId;
+    newMessage.getUserId = data.getUserId;
     newMessage.message = data.message;
-    newMessage.createdAt = new Date();
-    newMessage.updatedAt = new Date();
-    const result = await getRepository(Messages)
+    newMessage.createdAt = data.createdAt;
+    newMessage.updatedAt = data.updatedAt;
+    await getRepository(Messages)
       .createQueryBuilder()
       .insert()
       .values(newMessage)
@@ -30,29 +26,20 @@ export = {
       .where("Messages.sendUserId = :id", { id: userInfo.id })
       .getMany();
 
-    res.json(talkingUserObj(talkingUsers));
+    res.json({ result: talkingUserObj(talkingUsers), myId: userInfo.id });
   },
   getMessages: async (req: Request, res: Response) => {
     const userInfo = req.user;
     const toUserId = req.params.id;
-    console.log(Number(userInfo.id), Number(toUserId));
     const result = await getRepository(Messages)
       .createQueryBuilder("Messages")
-      .where("Messages.sendUserId = :sendId", { sendId: Number(userInfo.id) })
-      .andWhere("Messages.getUserId = :getId", { getId: Number(toUserId) })
+      .where(`Messages.sendUserId in (${userInfo.id},${toUserId})`)
+      .andWhere(`Messages.getUserId in (${userInfo.id},${toUserId})`)
+      .addOrderBy("Messages.createdAt", "ASC")
       .getMany();
-    console.log(result);
     res.json(result);
   }
 };
-
-async function getUserIdByEmail(email: string) {
-  return await getRepository(Users)
-    .createQueryBuilder()
-    .where("email = :email", { email: email })
-    .getOne();
-}
-
 function talkingUserObj(datas: any): Array<any> {
   var messageArray = [];
   var temp: Number[] = [];
@@ -61,6 +48,7 @@ function talkingUserObj(datas: any): Array<any> {
     var message: any = {};
     message.getUserId = datas[i].getUserId;
     message.user = datas[i].getUser.firstName + " " + datas[i].getUser.lastName;
+    message.email = datas[i].getUser.email;
     message.url = datas[i].getUser.profileImg;
     message.message = datas[i].message;
     messageArray.push(message);
